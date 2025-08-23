7846e9884b9ed77761e1028267eef50e5da00f67
MDAlviano <mdalviano.21122@gmail.com>
2025-08-23 15:46:01 +0700

    fix: fix join table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -37,6 +37,6 @@ class Product extends Model
     }
 
     public function types(): BelongsTo {
-        return $this->belongsTo(Product::class, "type_id", "id");
+        return $this->belongsTo(Type::class, "type_id", "id");
     }
 }
