97d7c0b39c9e3eda796f2fa99f611d4297d5a282
ibanez10 <ardhaneez@gmail.com>
2025-09-09 09:51:05 +0700

    kjads

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <title>Roccia - Admin</title>
 </head>
 <body class="flex min-h-screen bg-gray-100">
